76b8cdbacef7238f8e159e58d1c23e58da29f51a
Argyrys <rs9876555@gmail.com>
2026-08-17 20:08:06 +0530

    Add files via upload

---
+++ Conditional1.py
@@ -0,0 +1,7 @@
+#Check if a person is eligible for voting
+
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are eligible for voting.")
+else:
+    print("You are not eligible for voting.")
