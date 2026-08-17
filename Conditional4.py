f4dd5825dc6a63d4f6bf8e45492be384ee53a501
Argyrys <rs9876555@gmail.com>
2026-08-17 20:44:27 +0530

    Divisible by 5

---
+++ Conditional4.py
@@ -0,0 +1,8 @@
+#Check if a number is divisible by 5 or not
+ 
+
+n = int(input("Enter your Number:"))
+if n % 5 == 0:
+    print(f"{n} is divisible by 5.")
+else:
+    print(f"{n} is not divisible by 5.")
